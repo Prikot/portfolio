769d31e073af5b95a28b94cec9d9451cebf75db2
prikot <a.prikotov@gmail.com>
2016-12-13 13:00:00 +0300

    last changes in windows 01/12/16

---
+++ source/js/slider.js
@@ -0,0 +1,16 @@
+$)(function() {
+  $('.slider__controls-top').on('click', function (e) {
+    e.preventDefault();
+    
+    var markup = '<a class="newbie">Новая ссылка</a>'
+    $('.slider__controls').append(markup);
+    
+  });
+  
+  $('.newbie').on('click', function (e) {
+    e.preventDefault();
+
+    console.log('dfsfs');
+  });
+  
+}());
